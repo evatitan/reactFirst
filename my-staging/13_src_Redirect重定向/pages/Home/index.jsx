e98c7e77730de5466582c6f698c5b900d7d85b23
Evatitan <yinguihua2014@gmail.com>
2021-11-25 12:45:02 +0100

    use Redirect

---
+++ my-staging/13_src_Redirect重定向/pages/Home/index.jsx
@@ -0,0 +1,10 @@
+import React,{Component} from "react";
+
+export default class Home extends Component{
+    render(){
+        //console.log('Header component receive', this.props)
+        return (
+            <h3>I am Home component</h3>
+        )
+    }
+}
