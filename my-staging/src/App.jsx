95029f13394684c11402f84cc75d96e8dcfbebe0
Evatitan <yinguihua2014@gmail.com>
2021-12-10 19:52:36 +0100

    delete List and Search component

--- my-staging/src/App.jsx
+++ my-staging/src/App.jsx
@@ -1,18 +1,30 @@
-import React,{Component} from "react";
-import Search from "./components/Search";
-import List from "./components/List";
-
-
-
+import React, { Component } from 'react';
+import './App.css';
 export default class App extends Component {
-  render() {
-    return (
-      <div className="container">
-        <Search />
-        <List/>
-      </div>
-    );
-  }
-}
+  
+	componentDidMount() {
+		let loading = document.querySelector('.loading');
+		let letters = loading.textContent.split('');
+		loading.textContent = '';
+		letters.forEach((letter, i) => {
+			let span = document.createElement('span');
+			span.textContent = letter;
+			span.style.animationDelay = `${i / 5}s`;
+			loading.append(span);
+		});
 
- 
+		// let loading = document.querySelector('.loading');
+		// let letters = loading.textContent.split('');
+		// loading.textContent = '';
+		// letters.forEach((letter, i) => {
+		// 	let span = document.createElement('span');
+		// 	span.textContent = letter;
+		// 	span.style.animationDelay = `${i / 5}s`;
+		// 	loading.append(span);
+		// });
+	}
+
+	render() {
+		return <div class="loading">Loading</div>;
+	}
+}
